2b2138e0c9bf68dc0082aa65a1ce87c36c28960d
Chandresh Patidar <chandreshpatidar5@gmail.com>
2024-06-01 15:48:06 +0530

    Update chat message component to show amount, status, message and edit button

--- components/chat/ChatMessage.tsx
+++ components/chat/ChatMessage.tsx
@@ -1,36 +1,55 @@
 import React from 'react';
 import { DateTime } from 'luxon';
-import { SettlementStatus } from '@/typedefs/chat';
+import { Message } from '@/typedefs/chat';
+import { Pencil } from 'lucide-react';
 
 interface ChatMessageProps {
-  sender: boolean;
-  timestamp: string;
-  message?: string;
-  amount?: number;
-  status?: SettlementStatus;
+  message: Message;
+  userId: string;
+  showEditButton?: boolean;
+  onEdit?: () => void;
 }
 
 const ChatMessage: React.FC<ChatMessageProps> = (props) => {
-  const { message, sender, amount, timestamp, status } = props;
+  const { message, showEditButton, userId, onEdit } = props;
+
+  // Determine if the message is sent by the current user
+  const sender = message.sender === userId;
 
   return (
     <div
-      className={`relative px-4 py-2 rounded-md font-normal text-base text-white max-w-[70%] w-fit ${
-        sender ? ' self-end bg-cyan-900' : 'self-start bg-slate-700'
+      className={`relative px-4 py-2 rounded-md font-normal text-base text-white min-w-[250px] max-w-[70%] w-fit [&_.edit]:hover:block ${
+        sender ? 'self-end bg-cyan-900' : 'self-start bg-slate-700'
       }`}
     >
       <div className='flex flex-col gap-1'>
-        {status && <p className='z-10 capitalize'>Staus: {status} </p>}
-        {amount && <p className='z-10'>Amount: $ {amount} </p>}
-        {message && <p className='z-10 text-sm'>{message}</p>}
+        {/* Display message status if available */}
+        {message?.status && <p className='z-10 capitalize'>Status: {message.status} </p>}
+        {/* Display message amount if available */}
+        {message?.amount && <p className='z-10'>Amount: $ {message.amount} </p>}
+        {/* Display message content if available */}
+        {message?.message && <p className='z-10 text-sm'>{message.message}</p>}
       </div>
 
-      <small className='text-right block mt-4'>
-        <i>{DateTime.fromISO(timestamp).toRelative()}</i>
-      </small>
+      <div className='flex items-center justify-between mt-4 min-h-7'>
+        {/* Show edit button if the condition is met */}
+        {showEditButton && (
+          <button
+            onClick={onEdit}
+            className='edit hidden rounded-full p-1.5 bg-transparent text-white hover:text-slate-700 hover:bg-gray-200'
+            title='Edit'
+          >
+            <Pencil size={16} />
+          </button>
+        )}
+        {/* Display relative timestamp of the message */}
+        <small className='text-right flex-grow'>
+          <i>{DateTime.fromISO(message.timestamp).toRelative()}</i>
+        </small>
+      </div>
 
       <div
-        className={`absolute z-0 top-0 border-[24px] border-transparent rounded-md  ${
+        className={`absolute z-0 top-0 border-[24px] border-transparent rounded-md ${
           sender ? '-right-4 border-t-cyan-900' : '-left-4 border-t-slate-700'
         }`}
       />
