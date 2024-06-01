1258bc5a77cfedf1f995e4248731a5b577684692
Chandresh Patidar <chandreshpatidar5@gmail.com>
2024-06-01 16:22:30 +0530

    Setup socket.io

---
+++ server.js
@@ -0,0 +1,42 @@
+// Import necessary modules
+const http = require('http');
+const next = require('next');
+const SocketIO = require('socket.io');
+
+// Determine if the environment is development or production
+const dev = process.env.NODE_ENV !== 'production';
+// Define the hostname and port
+const hostname = 'localhost';
+const port = 3000;
+
+// Initialize the Next.js application with the specified settings
+const app = next({ dev: dev, hostname: hostname, port: port });
+const handler = app.getRequestHandler(); // Get the request handler for Next.js
+
+// Prepare the Next.js app
+app.prepare().then(function () {
+  // Create an HTTP server using the Next.js request handler
+  const httpServer = http.createServer(handler);
+
+  // Initialize Socket.IO with the HTTP server
+  const io = SocketIO(httpServer);
+
+  // Set up the connection event listener for Socket.IO
+  io.on('connection', (socket) => {
+    // Set up the disconnect event listener for the connected socket
+    socket.on('disconnect', () => {
+      // Add logic to handle disconnection
+    });
+  });
+
+  // Set up error handling for the HTTP server
+  httpServer
+    .once('error', function (err) {
+      console.error(err);
+      process.exit(1);
+    })
+    // Start the HTTP server and listen on the specified port
+    .listen(port, function () {
+      console.log('> Ready on http://' + hostname + ':' + port); // Log a message indicating the server is ready
+    });
+});
